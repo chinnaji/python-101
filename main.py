e73cd817df71c526be49b80c53270d3b8b24ded8
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-06-19 20:27:32 +0100

    created a function that open pages on browser

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+import requests
+import webbrowser
+
+# open pages on browser
+def open_page():
+    urls = ["https://chinnaji.vercel.app/","https://www.google.com/"]
+    for url in urls:
+        webbrowser.open(url)
+
+open_page()
